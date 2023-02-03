dc5887391b2fade34a291ee5d6b29ccc54272dd0
itzyashh <yashjadhav1411@rocketmail.com>
2023-02-04 05:11:55 +0530

    feat: add Searchbar
    added searchbar in HomeScreen

--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import React from "react"
-import { defaultStyle } from "../styles/styles"
+import { colors, defaultStyle } from "../styles/styles"
 import Header from "../components/Header"
+import { Avatar } from "react-native-paper"
 
 const Home = () => {
   return (
     <View style={defaultStyle}>
-      <Header emptyCart={true} />
+      <Header />
+      {/* Heading Row */}
+      <View
+        style={{
+          paddingTop: 70,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View>
+          <Text style={{ fontSize: 25 }}>Our</Text>
+          <Text style={{ fontSize: 25, fontWeight: "900" }}>Products</Text>
+        </View>
+        {/* SearchBar */}
 
-      <View>
-        <Text style={{ fontSize: 25 }}>Our</Text>
-        <Text style={{ fontSize: 25, fontWeight: "900" }}>Products</Text>
+        <View>
+          <TouchableOpacity>
+            <Avatar.Icon
+              icon={"magnify"}
+              size={50}
+              color={"gray"}
+              style={{ backgroundColor: colors.white, elevation: 12 }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          height: 80,
+        }}
+      ></View>
     </View>
   )
 }
--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -7,14 +7,14 @@ export const colors = {
   color1_light2: "rgba(199,0,73,0.8)",
   white: "white",
   color3: "rgb(45,45,45)",
-  color4: "transparent",
+  transparent: "transparent",
   color5: "#f2f2f2",
   color6: "#f7f7f7",
 }
 
 export const defaultStyle = StyleSheet.create({
   padding: 35,
-  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10 : 0,
   flex: 1,
   backgroundColor: colors.white,
 })
