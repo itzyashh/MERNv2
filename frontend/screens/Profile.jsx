c64f226bf18f35bec2838a018ab86692782a53d2
itzyashh <yashjadhav1411@rocketmail.com>
2023-02-17 08:21:04 +0530

    Update Profile.jsx

--- frontend/screens/Profile.jsx
+++ frontend/screens/Profile.jsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { colors, defaultStyle } from '../styles/styles'
 import FormHeading from '../components/miniComponents/FormHeading'
 import { Avatar } from 'react-native-paper'
+import Footer from '../components/Footer'
+import Spinner from 'react-native-loading-spinner-overlay/lib'
 
 const user = {
     name: "John Doe",
@@ -14,8 +16,43 @@ const user = {
     pincode: "123456",
 }
 
-const Profile = () => {
+const loading = false
+
+const Profile = ({navigation}) => {
+
+    const logoutHandler = () => {
+        console.log("logout")
+    }
+
+    const navigateHandler = (text) => {
+        switch (text) {
+           case 'Admin':
+            navigation.navigate('AdminPanel')
+            break;
+            case 'Orders':
+                navigation.navigate('Orders')
+                break;
+            case 'Profile':
+                navigation.navigate('UpdateProfile')
+                break;
+            case 'Password':
+                navigation.navigate('ChangePassword')
+                break;
+            case 'Log out':
+                logoutHandler()
+                break;
+            default:
+                navigation.navigate('Orders')
+                break;
+
+    }}
+
+    
   return (
+
+    <>
+    {loading &&  <Spinner size='large' color={colors.veryPeri}
+       visible={loading} textContent={"Loading..."} />}
     <View style={defaultStyle} >
         <View
         style={styles.container}
@@ -31,13 +68,37 @@ const Profile = () => {
         <View
         style={{flexDirection:'row',
                 margin:10,
+                marginTop:20,
                 justifyContent:"space-between",
         }}
         >
-
+            <ButtonBox handler={navigateHandler} icon='order-bool-descending' text={'Orders'}  />
+            <ButtonBox handler={navigateHandler} reverse icon='security' text={'Admin'} />
+            <ButtonBox handler={navigateHandler} icon='pencil-outline' text={'Profile'} />
+        </View>
+        <View
+        style={{flexDirection:'row',
+                margin:10,
+                marginTop:20,
+                justifyContent:"space-evenly",
+        }}
+        >
+            <ButtonBox handler={navigateHandler} icon='pencil' text={'Password'}  />
+            <ButtonBox handler={navigateHandler} icon='exit-to-app' text={'Log out'} />
         </View>
     </View>
+    <Footer />
+    </>
   )
+}
+
+const ButtonBox = ({icon,text,handler,reverse=false,loading=false}) => {
+    return (
+        <TouchableOpacity onPress={() =>handler(text)} disabled={loading} style={{ justifyContent:'space-around' ,backgroundColor:reverse?colors.veryPeri:colors.color3,height:74,width:74,borderRadius:5,alignItems: 'center',}}>
+            <Avatar.Icon size={50} icon={icon} style={{top:-7,alignSelf:"center",backgroundColor:colors.transparent,marginTop:10}} />
+            <Text  style={{top:-6,textAlign:"center",color:colors.white}}>{text}</Text>
+        </TouchableOpacity>
+    )
 }
 
 export default Profile
