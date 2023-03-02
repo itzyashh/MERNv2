801caa820904d0e88418ef4d6db1d992c3821913
itzyashh <yashjadhav1411@rocketmail.com>
2023-03-02 14:09:22 +0530

    create setMyProfile route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import {config} from 'dotenv';
-
+import userRouter from './routes/user.js'
 config(
     {
         path: './data/config.env'
     }
 );
-
-
-
-
 export const app = express();
+
+app.get('/',(req,res,next) => {
+    res.send('Hello World')
+})
+
+// Mounting the routers
+app.use('/api/v1/user',userRouter)
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import {app} from './app.js';
-import dotenv from 'dotenv';
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}, in ${process.env.NODE_ENV} mode`);
     });
