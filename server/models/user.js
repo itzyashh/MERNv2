c7eb6e7ba33165412bc722b42cb6200a255a9709
Yash Jadhav <yashjadhav1411@rocketmail.com>
2023-05-17 15:56:56 +0530

    feat: Add MongoDB user schema
    Added a new user schema to store user information in MongoDB. The schema includes fields for name, email, password, address, state, city, country, pincode, role, and avatar. This schema will be used to create user documents in the database.
    
    Changes Made:
    - Created a new MongoDB user schema with the specified fields.
    - Used Mongoose library to define and model the user schema.
    
    Co-authored-by:Yash

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,51 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import validator from "validator";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  pinCode: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  otp:Number,
+  otpExpires:Date,
+});
+
+export const User = mongoose.model("user", userSchema);
